7d1df74064911b7d2def4e5928f5a9431e32c804
BPLDuskRain <3108529508@qq.com>
2025-02-26 00:20:00 +0800

    1.2版本： 增加撞击生物击飞 优化矿车操作方式和效果

--- src/main/java/com/duskrainfall/betterminecart/CollisionListener.java
+++ src/main/java/com/duskrainfall/betterminecart/CollisionListener.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleBlockCollisionEvent;
+import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
 import org.bukkit.util.Vector;
 
 import java.util.logging.Level;
@@ -13,15 +14,31 @@ import static org.bukkit.Bukkit.getLogger;
 
 public class CollisionListener implements Listener {
     @EventHandler
-    public void whenCollision(VehicleBlockCollisionEvent e){
+    public void whenBlockCollision(VehicleBlockCollisionEvent e){
         if(!(e.getVehicle() instanceof Minecart minecart)) return;
         if(minecart.isEmpty()) return;
         if(!(minecart.getPassengers().get(0) instanceof Player)) return;
 //        getLogger().log(Level.INFO,"检测到碰撞" + e.getBlock().getType() + "在" + e.getBlock().getLocation());
-        Vector vector =  e.getVelocity();
+        Vector vector = e.getVelocity();
         vector.setX(-vector.getX());
         vector.setZ(-vector.getZ());
         minecart.setVelocity(vector);
 //        getLogger().log(Level.INFO,"速度改变");
+    }
+
+    @EventHandler
+    public void whenEntityCollision(VehicleEntityCollisionEvent e){
+        if(!(e.getVehicle() instanceof Minecart minecart)) return;
+        if(minecart.isEmpty()) return;
+        if(!(minecart.getPassengers().get(0) instanceof Player)) return;
+//        getLogger().log(Level.INFO, "检测到碰撞" + e.getEntity() + "在" + e.getEntity().getLocation());
+//        getLogger().log(Level.INFO, "矿车速度变为" + minecart.getVelocity());
+        Vector vector = minecart.getVelocity();
+
+        if(e.getEntity() instanceof Minecart minecart_attacked){
+            minecart_attacked.setMaxSpeed(2 * Math.max(vector.getX(), vector.getZ()));
+        }
+        e.getEntity().setVelocity(vector.setY(Math.abs(vector.getY()) + 1).multiply(2));
+        e.setCancelled(true);
     }
 }
--- src/main/java/com/duskrainfall/betterminecart/SpeedControllerListener.java
+++ src/main/java/com/duskrainfall/betterminecart/SpeedControllerListener.java
@@ -9,68 +9,77 @@ import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class SpeedControllerListener implements Listener {
     final public double MAX = 1.6d;
-    final public double MIN = 0.1d;
+    final public double MIN = 0.05d;
     final public Material CONTROL_ITEM = Material.RECOVERY_COMPASS;
 
     @EventHandler
     public void onClick(PlayerInteractEvent e){
         Player player = e.getPlayer();
         ItemStack item_main = player.getInventory().getItemInMainHand();
-        ItemStack item_off = player.getInventory().getItemInOffHand();
 
-        if(!player.isInsideVehicle()) return;
-        //副手加速，主手减速
-        if (item_off.getType() == CONTROL_ITEM) {
-            if(player.getVehicle() instanceof Minecart minecart){
-                minecart.setMaxSpeed(minecart.getMaxSpeed()*2);
-                if(minecart.getMaxSpeed() > MAX) minecart.setMaxSpeed(MAX);
+        if(item_main.getType() != CONTROL_ITEM) return; //必须手持对应物品
+        if(!player.isInsideVehicle()) return; //必须在载具内
+        if(!(player.getVehicle() instanceof Minecart minecart)) return; //载具必须是矿车
+        //左击空气加速，右击减速
+        switch(e.getAction()){
+            case Action.LEFT_CLICK_AIR :
+                if(minecart.getMaxSpeed() >= MAX) {
+                    minecart.setMaxSpeed(MAX);
+                    player.sendActionBar(Component.text("太快了，不能再加了！", NamedTextColor.RED));
+                }
+                else{
+                    minecart.setMaxSpeed(minecart.getMaxSpeed()*2);
+                    player.sendActionBar(Component.text("载具最大速度已经增加到 " +
+                            Math.min(minecart.getMaxSpeed(), 1.5d) + " block/tick", NamedTextColor.AQUA));
 
-                player.sendActionBar(Component.text("载具最大速度已经增加到 " + Math.min(minecart.getMaxSpeed(), 1.5d) + " block/tick", NamedTextColor.RED));
-//                player.sendMessage("载具最大速度已经增加到 " + minecart.getMaxSpeed() + " block/tick");
+                    minecart.getWorld().spawnParticle(
+                            Particle.FIREWORK, // 粒子类型
+                            minecart.getLocation().add(0, 1, 0), // 位置
+                            20, // 数量
+                            0.5, 0.5, 0.5, // 偏移量
+                            0.1 // 速度
+                    );
 
-                minecart.getWorld().spawnParticle(
-                        Particle.FIREWORK, // 粒子类型
-                        minecart.getLocation().add(0, 1, 0), // 位置
-                        20, // 数量
-                        0.5, 0.5, 0.5, // 偏移量
-                        0.1 // 速度
-                );
+                    player.playSound(
+                            player.getLocation(),
+                            Sound.ENTITY_FIREWORK_ROCKET_LAUNCH,
+                            8.0f,
+                            0.2f
+                    );
+                }
+                break;
+            case Action.RIGHT_CLICK_AIR:
+                if(minecart.getMaxSpeed() <= MIN) {
+                    minecart.setMaxSpeed(MIN);
+                    player.sendActionBar(Component.text("太慢了，不能再减了！", NamedTextColor.RED));
+                }
+                else{
+                    minecart.setMaxSpeed(minecart.getMaxSpeed()/2);
+                    player.sendActionBar(Component.text("载具最大速度已经限制到 " +
+                            minecart.getMaxSpeed() + " block/tick", NamedTextColor.AQUA));
 
-                player.playSound(
-                        player.getLocation(),
-                        Sound.ENTITY_FIREWORK_ROCKET_LAUNCH,
-                        8.0f,
-                        0.2f
-                );
-            }
-        }
-        else if (item_main.getType() == CONTROL_ITEM) {
-            if(player.getVehicle() instanceof Minecart minecart){
-                minecart.setMaxSpeed(minecart.getMaxSpeed()/2);
-                if(minecart.getMaxSpeed() < MIN) minecart.setMaxSpeed(MIN);
-                player.sendActionBar(Component.text("载具最大速度已经限制到 " + minecart.getMaxSpeed() + " block/tick", NamedTextColor.RED));
-//                player.sendMessage("载具最大速度已经限制到 " + minecart.getMaxSpeed() + " block/tick");
+                    minecart.getWorld().spawnParticle(
+                            Particle.LAVA, // 粒子类型
+                            minecart.getLocation().add(0, 1, 0), // 位置
+                            20, // 数量
+                            0.5, 0.5, 0.5, // 偏移量
+                            0.1 // 速度
+                    );
 
-                minecart.getWorld().spawnParticle(
-                        Particle.LAVA, // 粒子类型
-                        minecart.getLocation().add(0, 1, 0), // 位置
-                        20, // 数量
-                        0.5, 0.5, 0.5, // 偏移量
-                        0.1 // 速度
-                );
-
-                player.playSound(
-                        player.getLocation(),
-                        Sound.BLOCK_ANVIL_HIT,
-                        8.0f,
-                        0.2f
-                );
-            }
+                    player.playSound(
+                            player.getLocation(),
+                            Sound.BLOCK_ANVIL_HIT,
+                            8.0f,
+                            0.2f
+                    );
+                }
+                break;
         }
     }
 
