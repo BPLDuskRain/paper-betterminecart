23025749b140a38b4e059c727820c679811e4094
BPLDuskRain <3108529508@qq.com>
2025-03-29 00:16:22 +0800

    1.6.7版本 增强矿车风声 增加船水面跳跃 取消了滑行延迟 修复了滑行跳车时矿产继续飞行的问题 将降落模式修改为滑行模式

--- src/main/java/com/duskrainfall/betterminecart/vehicle/DriveListener.java
+++ src/main/java/com/duskrainfall/betterminecart/vehicle/DriveListener.java
@@ -1,8 +1,10 @@
 package com.duskrainfall.betterminecart.vehicle;
 
+import com.duskrainfall.betterminecart.vehicle.boat.Boats;
 import com.duskrainfall.betterminecart.vehicle.minecart.Minecarts;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Boat;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.minecart.RideableMinecart;
@@ -26,36 +28,64 @@ public class DriveListener implements Listener {
         if(item_main.getType() != Minecarts.CONTROL_ITEM) return;
         if(!player.isInsideVehicle()) return;
 
-        synchronized (player){
-            if(cds.containsKey(player)){
-                int ticks = player.getTicksLived() - cds.get(player);
-                if(ticks < CD){
-                    player.sendActionBar(Component.text("载具控制还有 " + (CD - ticks) + " 刻冷却时间！", NamedTextColor.RED));
-                    e.setCancelled(true);
-                    return;
+        Entity vehicleEntity = player.getVehicle();
+        if(vehicleEntity== null) return;
+
+        var action = e.getAction();
+        switch(vehicleEntity){
+            case RideableMinecart minecart -> {
+                synchronized (player){
+                    if(cds.containsKey(player)){
+                        int ticks = player.getTicksLived() - cds.get(player);
+                        if(ticks < CD){
+                            player.sendActionBar(Component.text("矿车控制还有 " + (CD - ticks) + " 刻冷却时间！", NamedTextColor.RED));
+                            e.setCancelled(true);
+                            return;
+                        }
+                    }
+                    cds.put(player, player.getTicksLived());
+                }
+                //左击加速，右击空气减速
+                switch(action){
+                    case Action.LEFT_CLICK_AIR, Action.LEFT_CLICK_BLOCK -> {
+                        Minecarts.maxSpeedUp(minecart, player);
+                        e.setCancelled(true);
+                    }
+                    case Action.RIGHT_CLICK_AIR -> {
+                        Minecarts.maxSpeedDn(minecart, player);
+                        e.setCancelled(true);
+                    }
+                    default -> {
+                        synchronized (player){
+                            cds.put(player, player.getTicksLived() - CD);
+                        }
+                    }
                 }
             }
-            cds.put(player, player.getTicksLived());
-        }
-
-        Entity vehicleEntity = player.getVehicle();
-        if(vehicleEntity instanceof RideableMinecart minecart){
-            //左击加速，右击空气减速
-            switch(e.getAction()){
-                case Action.LEFT_CLICK_AIR:
-                case Action.LEFT_CLICK_BLOCK:
-                    Minecarts.maxSpeedUp(minecart, player);
-                    e.setCancelled(true);
-                    break;
-                case Action.RIGHT_CLICK_AIR:
-                    Minecarts.maxSpeedDn(minecart, player);
-                    e.setCancelled(true);
-                    break;
-                default:
-                    synchronized (player){
-                        cds.put(player, player.getTicksLived() - CD);
+            case Boat boat -> {
+                synchronized (player){
+                    if(cds.containsKey(player)){
+                        int ticks = player.getTicksLived() - cds.get(player);
+                        if(ticks < CD){
+                            player.sendActionBar(Component.text("立体机动还有 " + (CD - ticks) + " 刻冷却时间！", NamedTextColor.RED));
+                            e.setCancelled(true);
+                            return;
+                        }
                     }
+                    cds.put(player, player.getTicksLived());
+                }
+                switch (action) {
+                    case Action.LEFT_CLICK_AIR, Action.LEFT_CLICK_BLOCK -> {
+                        e.setCancelled(true);
+                    }
+                    case Action.RIGHT_CLICK_AIR, Action.RIGHT_CLICK_BLOCK -> {
+                        Boats.boatUp(boat, player);
+                        e.setCancelled(true);
+                    }
+                    default -> {}
+                }
             }
+            default -> {}
         }
     }
 }
